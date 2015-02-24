633197f6ba7572db951255d2e204d291edca6ed0
yakexinpy <yakexittmtt@gmail.com>
2015-02-24 14:56:18 -0800

    Create 150 Evaluate Reverse Polish Notation.java

---
+++ 150 Evaluate Reverse Polish Notation.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public int evalRPN(String[] tokens) {
+        if(tokens.length < 1) return 0;
+        Stack<Integer> stack = new Stack<Integer>();
+        
+        for(int i = 0; i < tokens.length; i++){
+            if(tokens[i].equals("+") || tokens[i].equals("-") || tokens[i].equals("*")||tokens[i].equals("/")){
+                int b = stack.isEmpty() ? 0 : stack.pop();
+                int a = stack.isEmpty() ? 0 : stack.pop();
+                if(tokens[i].equals("+"))
+                    stack.push(a + b);
+                else if(tokens[i].equals("-"))
+                    stack.push(a - b);
+                else if(tokens[i].equals("*"))
+                    stack.push(a * b);
+                else if(tokens[i].equals("/")){
+                    if(b == 0)
+                        stack.push(0);
+                    else
+                        stack.push(a / b);
+                }
+            }
+            else{
+                stack.push(Integer.parseInt(tokens[i]));
+            }
+        }
+        return stack.pop();
+    }
+}
