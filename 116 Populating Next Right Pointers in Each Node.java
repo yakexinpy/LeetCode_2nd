027d355e933b15b8e28e69e8a475b705fe91c641
yakexinpy <yakexittmtt@gmail.com>
2015-03-01 21:43:56 -0800

    Update 116 Populating Next Right Pointers in Each Node.java

--- 116 Populating Next Right Pointers in Each Node.java
+++ 116 Populating Next Right Pointers in Each Node.java
@@ -9,30 +9,17 @@
 public class Solution {
     public void connect(TreeLinkNode root) {
         if(root == null) return;
-        Queue<TreeLinkNode> Q = new LinkedList<TreeLinkNode>();
-        Q.offer(root);
-        int curLev = 1, nextLev = 0;
         
-        while(!Q.isEmpty()){
-            TreeLinkNode node = Q.poll();
-            curLev--;
-            if(node.left != null){
-                Q.offer(node.left);
-                nextLev++;
+        TreeLinkNode cur;
+        while(root.left != null){
+            cur = root;
+            while(cur != null){
+                cur.left.next = cur.right;
+                if(cur.next != null)
+                    cur.right.next = cur.next.left;
+                cur = cur.next;
             }
-            if(node.right != null){
-                Q.offer(node.right);
-                nextLev++;
-            }
-            
-            if(curLev == 0){
-                node.next = null;
-                curLev = nextLev;
-                nextLev = 0;
-            }
-            else{
-                node.next = (Q.isEmpty()) ? null : Q.peek();    
-            }
+            root = root.left;
         }
     }
 }
