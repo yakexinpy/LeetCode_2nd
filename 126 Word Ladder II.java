5b6e76b9e7d3467c9518f8962576f861617cf2d0
yakexinpy <yakexittmtt@gmail.com>
2015-03-04 23:45:27 -0800

    Update 126 Word Ladder II.java

--- 126 Word Ladder II.java
+++ 126 Word Ladder II.java
@@ -1,102 +1,91 @@
 public class Solution {
     class Node{
-        public Node parent;
-        public int curLev;
-        public String str;
-        
-        public Node(Node n, String s, int l){
-            parent = n;
-            curLev = l;
+        int level;
+        String str;
+        public Node(int l, String s){
+            level = l;
             str = s;
         }
     }
     
-    public void calNeighbors(Set<String> dict, String str, HashMap<String, HashSet<String>> neighbors){
-        char[] chs = str.toCharArray();
-        
-        for(int i = 0; i < str.length(); i++){
-            char ch = chs[i];
-            for(char c = 'a'; c <= 'z'; c++){
-                if(c == ch) continue;
-                chs[i] = c;
-                String newStr = new String(chs);
-                if(dict.contains(newStr)){
-                    HashSet<String> set = neighbors.get(str);
-                    if(set != null) 
-                        set.add(newStr);
-                    else{
-                        set = new HashSet<String>();
-                        set.add(newStr);
-                        neighbors.put(str, set);
-                    }
-                }
-            }
-            chs[i] = ch;
-        }
-    }
-    
-    public void findPath(Node n, List<List<String>> ret){
-        List<String> res = new ArrayList<String>();
-        Node p = n;
-        
-        while(p != null){
-            res.add(0, p.str);
-            p = p.parent;
-        }
-        ret.add(res);
-    }
-    
     public List<List<String>> findLadders(String start, String end, Set<String> dict) {
         List<List<String>> ret = new ArrayList<List<String>>();
-       
-        dict.add(start);
-        dict.add(end);
-               
-        HashMap<String, HashSet<String>> map = new HashMap<String, HashSet<String>>(); 
-        for(String s : dict){
-            calNeighbors(dict, s, map);
-        }
+        Set<String> unvisited = new HashSet<String>();
+        unvisited.addAll(dict);
+        unvisited.add(start);
+        unvisited.remove(end);
         
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        for(String s : unvisited)
+            map.put(s, new ArrayList<String>());
+            
         Queue<Node> q = new LinkedList<Node>();
-        q.add(new Node(null, start, 1));
+        q.add(new Node(0, end));
         
-        Map<String, Integer> visited = new HashMap<String, Integer>();
-        int pre = 0;
+        int curLev = 0, preLev = 0, lastLev = Integer.MAX_VALUE;
+        boolean found = false;
+        Set<String> visitedCurLev = new HashSet<String>();
         
         while(!q.isEmpty()){
-            Node n = q.poll();
-            if(n.str.equals(end)){
-                if(pre == 0 || n.curLev == pre){
-                    pre = n.curLev;
-                    findPath(n, ret);
-                }
-                else
-                    break;
-            }
-            else{
-                HashSet<String> set = map.get(n.str);
-                if(set == null || set.isEmpty()) continue;
-                
-                List<String> remove = new ArrayList<String>();
-                for(String s : set){
-                    if(visited.containsKey(s)){
-                        int level = visited.get(s);
-                        if(n.curLev + 1 > level){
-                            map.get(s).remove(n.str);
-                            remove.add(s);
-                            continue;
+            Node node = q.poll();
+            curLev = node.level;
+            String curStr = node.str;
+            
+            if(found && curLev > lastLev) break;
+            
+            if(curLev > preLev)
+                unvisited.removeAll(visitedCurLev);
+            
+            preLev = curLev;
+            char[] chs = curStr.toCharArray();
+            for(int i = 0; i < chs.length; i++){
+                char old = chs[i];
+                boolean foundCur = false;
+                for(char c = 'a'; c <= 'z'; c++){
+                    if(c == old) continue;
+                    chs[i] = c;
+                    String newStr = new String(chs);
+                    if(unvisited.contains(newStr)){
+                        map.get(newStr).add(curStr);
+                        if(newStr.equals(start)){
+                            found = true;
+                            lastLev = curLev;
+                            foundCur = true;
+                            break;
+                        }
+                        
+                        if(!visitedCurLev.contains(newStr)){
+                            visitedCurLev.add(newStr);
+                            q.add(new Node(curLev + 1, newStr));
                         }
                     }
-                        visited.put(s, n.curLev + 1);
-                        q.add(new Node(n, s, n.curLev + 1));
-                        if(map.containsKey(s))
-                            map.get(s).remove(n.str);
                 }
-                for(String s : remove)
-                    set.remove(s);
+                if(foundCur) break;
+                chs[i] = old;
             }
+        }
+        
+        if(found){
+            List<String> tmp = new ArrayList<String>();
+            tmp.add(start);
+            buildPath(start, end, tmp, ret, map, lastLev + 1);
         }
         
         return ret;
+    }
+    
+    private void buildPath(String start, String end, List<String> tmp, List<List<String>> ret, Map<String ,List<String>> map, int curLev){
+        if(start.equals(end)){
+            ret.add(new ArrayList<String>(tmp));
+            return;
+        }
+        
+        if(curLev <= 0) return;
+        
+        for(String s : map.get(start)){
+            tmp.add(s);
+            buildPath(s, end, tmp, ret, map, curLev - 1);
+            tmp.remove(tmp.size() - 1);
+        }
     }
 }
